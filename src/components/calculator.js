b990907aed141b8dfb69157375dfd83df7bd7aaf
jeffwgates <jeff.gates0507@gmail.com>
2020-07-02 00:23:36 -0600

    Added new total

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,4 +1,13 @@
+
+// 1) User clicks first number, store in integer, show number ---
+// 2) User clicks plus or minus, store user choice, ---
+// 3) User clicks second number, store first number in newNum, store second number in intEger, show intEger
+// 4) User clickes equals button, perform doOpration, show total
+
+
+
 import React, { Component } from "react";
+
 
 
 export default class Calculator extends Component {
@@ -6,9 +15,9 @@ export default class Calculator extends Component {
     super();
 
     this.state = {
-   
       intEger: 0,
-      newNumber: 0
+      newNumber: 0,
+      operation: ""
     };
 
     this.punchedNumber = this.punchedNumber.bind(this);
@@ -16,25 +25,50 @@ export default class Calculator extends Component {
 
  
   punchedNumber(newEntry) {
-    this.setState({intEger: newEntry})
-    console.log(this.state.intEger)
- 
+    let tempNumber = this.state.intEger;
+    this.setState({
+      intEger: newEntry, 
+      newNumber: tempNumber,
+    });
   }
 
 
-  newSetNumber= () => {
-    this.setState({newNumber: event.target.value})
-   
+
+  operationPlus = () => {
+    this.setState({operation: "plus"})
+  }
+
+
+  operationMinus = () => {
+    this.setState({operation: "minus"})
+  }
+
+  doOperation = () => {
+    let total = 0;
+    if(this.state.operation == "plus"){
+      total = (+this.state.intEger) + (+this.state.newNumber);
+    } else if (this.state.operation == "minus"){
+      //TODO: the line after this is a problem, can you tell me why?
+      total = +this.state.intEger - +this.state.newNumber;
+    }
+    //return "total";
+    //TODO: There is a problem - you need to add a line here
+    //Add something here to fix it
+    this.setState({intEger:total})
+    
   }
 
 
 
   render () {
+   
     return (
-
-        
-      <div className='calculator-wrapper'>   
-       <input style={{count: this.state.NewNumber}} placeholder = '0' value={this.state.intEger} onChange={event => this.newSetNumber()}/>
+      <div className='calculator-wrapper'>
+        {this.state.operation}
+       <input placeholder = '0' value={this.state.intEger}/>
+       <button onClick={() => this.doOperation()}>Equals</button>
+       <button onClick={() => this.operationPlus()}>Plus</button>
+       <button onClick={() => this.operationMinus()}>Minus</button>
        <button onClick={() => this.punchedNumber("0")}>0</button>
        <button onClick={() => this.punchedNumber("1")}>1</button>
        <button onClick={() => this.punchedNumber("2")}>2</button>
@@ -45,10 +79,12 @@ export default class Calculator extends Component {
        <button onClick={() => this.punchedNumber("7")}>7</button>
        <button onClick={() => this.punchedNumber("8")}>8</button>
        <button onClick={() => this.punchedNumber("9")}>9</button>
+       
       </div>
 
 
 
     );
+    //[1,2,3,4,5].map()
   }
 }
